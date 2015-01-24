501f72357562ac247317674400aba76b894a2ddd
zijianyue <zijianyue@163.com>
2015-01-24 13:20:01 +0800

    增加-include选项和去掉error限制

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,6 +4,7 @@
 									"-Ie:/projects/eNavi2_800X480_ChangeUI/upf/"
 									"-Ie:/projects/eNavi2_800X480_ChangeUI/upf_dubhe/export/"
 									"-Ie:/projects/eNavi2_800X480_ChangeUI/UPF_SMI/Include/"
+									"-include/Service/TG/MM/RM/Include/StdAfx.h"
 									))
 		 (flycheck-clang-include-path . ("e:/projects/eNavi2_800X480_ChangeUI/"
 										"e:/projects/eNavi2_800X480_ChangeUI/include/"
