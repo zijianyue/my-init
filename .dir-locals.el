33e367782a5d13dfabe97d4348676ccc9b55b0b7
zijianyue <zijianyue@163.com>
2015-01-24 10:04:42 +0800

    for clang complete and flycheck

---
+++ .dir-locals.el
@@ -0,0 +1,17 @@
+((nil . ((company-clang-arguments . ("-Ie:/projects/eNavi2_800X480_ChangeUI/"
+									"-Ie:/projects/eNavi2_800X480_ChangeUI/include/"
+									"-Ie:/projects/eNavi2_800X480_ChangeUI/server/"
+									"-Ie:/projects/eNavi2_800X480_ChangeUI/upf/"
+									"-Ie:/projects/eNavi2_800X480_ChangeUI/upf_dubhe/export/"
+									"-Ie:/projects/eNavi2_800X480_ChangeUI/UPF_SMI/Include/"
+									))
+		 (flycheck-clang-include-path . ("e:/projects/eNavi2_800X480_ChangeUI/"
+										"e:/projects/eNavi2_800X480_ChangeUI/include/"
+										"e:/projects/eNavi2_800X480_ChangeUI/server/"
+										"e:/projects/eNavi2_800X480_ChangeUI/upf/"
+										"e:/projects/eNavi2_800X480_ChangeUI/upf_dubhe/export/"
+										"e:/projects/eNavi2_800X480_ChangeUI/UPF_SMI/Include/"
+										))
+		 )))
+
+
