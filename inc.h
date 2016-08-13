f01a2dc1657825381978169b609806591f273528
Ge Zijian <zijianyue@163.com>
2016-08-13 10:13:11 +0800

    Create inc.h

---
+++ inc.h
@@ -0,0 +1,18 @@
+typedef struct TAG_TYPE
+{
+    int sbl;
+    short abb;
+}TAG_TYPE_S;
+typedef struct TTT
+{
+    long ttt;
+}TTT_S;
+
+typedef struct DDD
+{
+    long ddd;
+}DDD_S;
+#define    PRIORITY     0x00000001 
+
+#define    JOB_ID_INIT                   0x00000000 
+typedef void* JOB_ID;
